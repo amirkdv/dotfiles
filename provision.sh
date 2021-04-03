3ab87c6c8a5251eb2d26535681e2074ca7125bab
Amir Kadivar <amir@amirkdv.ca>
2021-04-03 16:45:38 -0400

    add provision.sh

---
+++ provision.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set -euox pipefail
+
+sudo apt-get update -qq
+
+# generic tools
+sudo apt-get install -qqy \
+  make \
+  vim \
+  git \
+  wget \
+  curl \
+  ncdu \
+  patch \
+  tree \
+  zip \
+  unzip \
+  gzip \
+  bzip2 \
+  rsync \
+  locales \
+  tzdata
+
+# dev environment tools
+sudo apt-get install -qqy \
+  gcc \
+  build-essential \
+  tmux \
+  python3 python3-dev python3-pip \
+  sqlite3 \
+  graphviz libgraphviz-dev \
+  texlive texlive-xetex \
+  pandoc \
+  inkscape
